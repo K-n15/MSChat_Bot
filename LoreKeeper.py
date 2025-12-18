9a66e5008682e84018f1f3eabc65342fe8983bba
K-n15 <theepapon7055@gmail.com>
2025-12-18 14:20:50 +0700

    Try to make result appear in log

--- LoreKeeper.py
+++ LoreKeeper.py
@@ -1,4 +1,4 @@
-import  os, json, logging, hashlib, sys
+import  os, json, logging, hashlib, sys, requests
 from flask import Flask, request
 from dotenv import load_dotenv
 
@@ -13,16 +13,6 @@ logging.basicConfig(
 # Use for debug
 # logging.basicConfig(filename='myapp.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-@app.route("/webhook",methods=['POST'])
-def ReceiveWebhook():
-    body = request.json
-    if body["object"] == "page":
-        logger.info('start page logging')
-        logger.info(body["entry"])
-        logger.info('end page logging')
-        return "EVENT_RECEIVED",200
-    return "PAGE_NOT_FOUND",404
 
 @app.route("/webhook",methods=['GET'])
 def Verify():
@@ -40,3 +30,39 @@ def Verify():
 @app.route("/wakeup",methods=['GET'])
 def WakeupCall():
     return "Wake up call, OK", 200
+
+@app.route("/webhook",methods=['POST'])
+def ReceiveWebhook():
+    body = request.json
+    if body["object"] == "page":
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", file=sys.stdout)
+        print("I RECEIVED A MESSAGE!", file=sys.stdout)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", file=sys.stdout)
+        logger.info('start page logging')
+        logger.info(body["entry"])
+        logger.info('end page logging')
+        return "EVENT_RECEIVED",200
+    return "PAGE_NOT_FOUND",404
+
+def send_message(recipient_id, message_text):
+
+    logger.info("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        err = str(r.status_code) + " " + r.text
+        logger.info(err)
